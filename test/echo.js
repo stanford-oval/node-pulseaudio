9c06c0996b29a5904e1fe2f889c5372d63c38696
Kayo Phoenix <kayo@illumium.org>
2013-10-07 18:56:56 +0600

    Added simple echo test

---
+++ test/echo.js
@@ -0,0 +1,34 @@
+var Pulse = require('..');
+
+var ctx = new Pulse({
+  client: 'test-client'
+});
+
+ctx.on('state', function(state){
+  console.log('context:', state);
+});
+
+ctx.on('connection', function(){
+  var opts = {
+    channels:1,
+    rate:8000,
+    format:'s16le'
+  };
+
+  var rec = ctx.createRecordStream(opts),
+      play = ctx.createPlaybackStream(opts);
+
+  rec.on('state', function(state){
+    console.log('record:', state);
+  });
+  play.on('state', function(state){
+    console.log('playback:', state);
+  });
+
+  rec.pipe(play);
+
+  setTimeout(function(){
+    rec.end();
+    play.end();
+  }, 5000);
+});
