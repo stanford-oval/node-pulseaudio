6469d02b276aab5738f46e603d229c4bf933dde5
ad31c0 <78684321+ad31c0@users.noreply.github.com>
2021-04-04 21:18:10 +0200

    better test for module capabitilities (#35)
    Co-authored-by: ad31c0 <ad31c0@users.noreply.github.com>

--- test/module.js
+++ test/module.js
@@ -7,30 +7,31 @@ async function main() {
         client: 'test-client',
     });
 
+    let testModule = "module-null-sink";
     let oModulesList;
 
     oModulesList = await ctx.modules();
     for (let i = 0; i < oModulesList.length; i++) {
         const m = oModulesList[i];
-        if (m.name === "module-null-sink") {
-            console.log("remove module-null-sink");
+        if (m.name === testModule) {
+            console.log("remove", testModule);
             await ctx.unloadModule(m.index);
         }
     }
 
     oModulesList = await ctx.modules();
     oModulesList.forEach((m) => {
-        if (m.name === "module-null-sink")
-            throw new Error("failed to remove module-null-sink");
+        if (m.name === testModule)
+            throw new Error("failed to remove " + testModule);
     });
 
-    let result = await ctx.loadModule("module-null-sink");
-    console.log("load module, index", result);
+    let result = await ctx.loadModule(testModule);
+    console.log("load", testModule, "index", result);
 
     oModulesList = await ctx.modules();
     oModulesList.forEach((m) => {
-        if (m.name === "module-null-sink")
-            console.log("module found");
+        if (m.name === testModule)
+            console.log("module", testModule, "found");
     });
 
     ctx.end();
